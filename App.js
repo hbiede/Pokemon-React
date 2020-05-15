e62ced87a2da9c813008a61fd0b3e22ce3dfe0dd
hbiede_dmsi <hbiede@dmsi.com>
2020-05-15 13:56:56 -0500

    Pulls Pokemon Names from the PokiAPI and displays them in a dropdown

--- App.js
+++ App.js
@@ -1,109 +1,102 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Pokemon Battle app built in React Native
  *
  * @format
  * @flow strict-local
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Text, View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Picker} from '@react-native-community/picker';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class PokemonPickerView extends React.Component {
+  state = {
+    value: 'key1',
+  };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: [],
+      error: null,
+      isLoaded: false,
+      selectedPokemon: null,
+    };
+  }
+
+  async componentDidMount() {
+    let pokemonFetched = [];
+    for (let i = 1; i < 152 && this.state.error == null; i++) {
+      await fetch('https://pokeapi.co/api/v2/pokemon/' + i)
+        .then((result) => result.json())
+        .then(
+          (result) => pokemonFetched.push(result),
+          (error) =>
+            this.setState({
+              isLoaded: true,
+              error: error,
+            }),
+        );
+    }
+
+    // Not sure how else to do this:
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({
+      pokemon: pokemonFetched,
+      isLoaded: true,
+    });
+  }
+
+  render(): View {
+    if (this.state.error) {
+      return <Text>{this.state.error}</Text>;
+    } else if (this.state.isLoaded) {
+      let pokemonListEntries = this.state.pokemon.map((s, i) => {
+        return (
+          <Picker.Item
+            label={s.name[0].toUpperCase() + s.name.substring(1)}
+            value={s.name}
+            key={i}
+          />
+        );
+      });
+
+      if (pokemonListEntries.length > 0) {
+        if (!this.state.selectedPokemon) {
+          this.state.selectedPokemon = pokemonListEntries[0];
+        }
+        return (
+          <View>
+            <Picker
+              selectedValue={this.state.selectedPokemon}
+              onValueChange={(pokemon) =>
+                this.setState({selectedPokemon: pokemon})
+              }>
+              {pokemonListEntries}
+            </Picker>
+            <Button title="Battle!" />
+          </View>
+        );
+      } else {
+        return <Text>No Pokemon found</Text>;
+      }
+    } else {
+      return <Text>Loading...</Text>;
+    }
+  }
+}
+
+const Stack = createStackNavigator();
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Pick your Pokemon" component={PokemonPickerView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
